218b6c705f326328b413bc988625ed33971c8d2c
ThubeesanKugathas <Thubeesan@outlook.com>
2021-12-14 15:34:58 +0100

    title js test

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 // numero di array per contenuto: 3
 
 // array per il titolo
+
 const arrayTitles = ['Title1', 'Title2', 'Title3', 'Title4'];
 // array per il contenuto 
-const arrayContents = ['Content1', 'Content2', 'Content3', 'Content4'];
+// const arrayContents = ['Content1', 'Content2', 'Content3', 'Content4'];
 // array per le immagini
-
 
 // current index counter 
 let currentIndex = 0;
@@ -13,14 +13,23 @@ let currentIndex = 0;
 // definire zona titolo
 const title = document.querySelector('.ms_title');
 
+
 // definire zona contenuti
-const content = document.querySelector('.ms_content');
+// const content = document.querySelector('.ms_content');
+
+title.innerHTML = arrayTitles[currentIndex];
 
 // creare due button per next ed prev 
 const buttonNext = document.querySelector('.ms_button-next');
 const buttonPrev = document.querySelector('.ms_button-prev');
 
 buttonNext.addEventListener('click', function() {
-    currentIndex++;
+    currentIndex++; 
 
+    if (currentIndex > arrayTitles.length - 1) {
+        currentIndex = 0;
+    }
+
+    title.innerHTML = arrayTitles[currentIndex];
 })
+
